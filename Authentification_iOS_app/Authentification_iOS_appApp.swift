f4e017ee6f78ec8bfa6d3669ec47aed06566b192
kramarivan <99kramar@gmail.com>
2024-11-15 12:39:13 +0100

    Initial Commit

---
+++ Authentification_iOS_app/Authentification_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Authentification_iOS_appApp.swift
+//  Authentification_iOS_app
+//
+//  Created by Ivan Kramar on 15.11.2024..
+//
+
+import SwiftUI
+
+@main
+struct Authentification_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Authentification_iOS_appTests/Authentification_iOS_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Authentification_iOS_appTests.swift
+//  Authentification_iOS_appTests
+//
+//  Created by Ivan Kramar on 15.11.2024..
+//
+
+import Testing
+@testable import Authentification_iOS_app
+
+struct Authentification_iOS_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
